4e170162b4524bba23b1ab1b8b43cb7e861ea063
rosstafarian <rosstafarian1@gmail.com>
2018-05-11 23:21:00 -0500

    Blade includes cleanup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,35 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>{{ $title }}</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/fonts.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/crumina-fonts.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/normalize.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/grid.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/styles.css') }}">
-
-    <!--Plugins styles-->
-
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/jquery.mCustomScrollbar.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/swiper.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/primary-menu.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/magnific-popup.css') }}">
-
-    <!--Styles for RTL-->
-
-    <!--<link rel="stylesheet" type="text/css" href="app/css/rtl.css">-->
-
-    <!--External fonts-->
-
-    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
-    <style>
-        .padded-50 {
-            padding: 40px;
-        }
-
-        .text-center {
-            text-align: center;
-        }
-    </style>
+    @include('includes.styles')
 
 </head>
 
@@ -365,16 +337,7 @@
 
 <!-- JS Script -->
 
-<script src="{{ asset('app/js/jquery-2.1.4.min.js') }}"></script>
-<script src="{{ asset('app/js/crum-mega-menu.js') }}"></script>
-<script src="{{ asset('app/js/swiper.jquery.min.js') }}"></script>
-<script src="{{ asset('app/js/theme-plugins.js') }}"></script>
-<script src="{{ asset('app/js/main.js') }}"></script>
-<script src="{{ asset('app/js/form-actions.js') }}"></script>
-<script src="{{ asset('app/js/velocity.min.js') }}"></script>
-<script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
-<script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
-<script id="dsq-count-scr" src="//lavablog.disqus.com/count.js" async></script>
+@include('includes.scripts')
 
 <!-- ...end JS Script -->
 
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,34 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>{{ $title }}</title>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/fonts.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/crumina-fonts.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/normalize.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/grid.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/styles.css') }}">
-
-    <!--Plugins styles-->
-
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/jquery.mCustomScrollbar.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/swiper.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/primary-menu.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('app/css/magnific-popup.css') }}">
-
-    <!--Styles for RTL-->
-
-    <!--<link rel="stylesheet" type="text/css" href="app/css/rtl.css">-->
-
-    <!--External fonts-->
-
-    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
-    <style>
-        .padded-50{
-            padding: 40px;
-        }
-        .text-center{
-            text-align: center;
-        }
-    </style>
+    @include('includes.styles')
 
 </head>
 
@@ -86,16 +59,15 @@
     </symbol>
 </svg>
 
+<!-- Overlay Search -->
+
+@include('includes.search')
+
+<!-- End Overlay Search -->
+
 <!-- JS Script -->
 
-<script src="{{ asset('app/js/jquery-2.1.4.min.js') }}"></script>
-<script src="{{ asset('app/js/crum-mega-menu.js') }}"></script>
-<script src="{{ asset('app/js/swiper.jquery.min.js') }}"></script>
-<script src="{{ asset('app/js/theme-plugins.js') }}"></script>
-<script src="{{ asset('app/js/main.js') }}"></script>
-<script src="{{ asset('app/js/velocity.min.js') }}"></script>
-<script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
-<script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
+@include('includes.scripts')
 
 <!-- ...end JS Script -->
 
